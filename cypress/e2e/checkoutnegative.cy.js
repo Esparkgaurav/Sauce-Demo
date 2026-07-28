74ac71853b16be95fa18e214f150df5e309f695d
Gaurav Sonar <boyonjourney29@gmail.com>
2026-07-28 17:46:22 +0530

    checkout negative flow added commit

--- cypress/e2e/checkoutnegative.cy.js
+++ cypress/e2e/checkoutnegative.cy.js
@@ -14,7 +14,6 @@ describe("Checkout Negative", ()=>{
             inventorypage.addproduct();
             inventorypage.clickcartbadge();
             yourcartpage.clickcheckoutbtn();
-            checkoutpage.enterinfo(" "," "," ");
             checkoutpage.clickcontinubtn();
             checkoutpage.withoutinfo();
             
